6c0f2ac9e9987c681b3f2f51731e1b7cfea08da5
KB <popotwtw@gmail.com>
2018-12-22 02:32:11 +0800

    Add files via upload

---
+++ click_echo.py
@@ -0,0 +1,5 @@
+# click.echo() v.s print() -> 1. echo() has no return value, print has return value (1)
+#                             2. echo() can be called consecutively but print() can't
+#                                Ex: echo('a')('b')('c')
+#                             3. echo() is slightly faster than print()
+#                             4. echo() does not exist in python (but click.echo does)
