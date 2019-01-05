b409f787d1c0fc007899f030af84cb6a6175330a
KB <popotwtw@gmail.com>
2019-01-05 15:20:12 +0800

    Update basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -47,4 +47,20 @@ print(max(10, 3))  # output 10
 
 >>> zip(a, c) # Result = [(1, 4), (2, 5), (3 ,6)] 
 >>> zip(*zipped) # *zipped -> unzipped， return a two-dimensional array [(1, 2, 3), (4, 5, 6)]
+
+Ex: https://github.com/KBLin1996/NIAC_FaceRecognition/blob/master/opencv.py
+for(top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 '''
